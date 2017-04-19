04c7fa40094aae8232a0fc4cee4e9ee9454db78e
iggytoto <iggytoto@gmail.com>
2017-04-19 23:12:44 +0300

    Initial commit, blank app

---
+++ desktop/src/game/crb/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package game.crb.desktop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(){
+
+			@Override
+			public void create() {
+				
+			}
+		}, config);
+	}
+}
