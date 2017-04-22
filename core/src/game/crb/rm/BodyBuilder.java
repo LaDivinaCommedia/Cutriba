5748db7cdb9b1d3d50fcce17eb9761f64f669e55
Victorius <ricka_elf@mail.ru>
2017-04-22 23:52:15 +0300

    Added level "Just do it"

--- core/src/game/crb/rm/BodyBuilder.java
+++ core/src/game/crb/rm/BodyBuilder.java
@@ -145,6 +145,7 @@ public class BodyBuilder {
                 Body body = getRectangle(rect, physics);
 
                 body.getFixtureList().get(0).setUserData("finish");
+
             }
         }
     }
